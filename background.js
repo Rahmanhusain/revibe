9734d23aab5ba276189b407650ff511509366a1b
Rahmanhusain <rahmanhusain899@gmail.com>
2025-09-15 20:10:27 +0530

    UBOL_seperation

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 // Function to update the totalDuration value which tracks the user's screen time and reset every day change
-import '/js/background.js'
+import '/UBOL_codebase/js/background.js'
 
 //Start of totalDuration
 
